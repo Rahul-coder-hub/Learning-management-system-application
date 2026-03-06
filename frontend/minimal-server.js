0be1f1bfa239ddb6c4614919b8674f9af361a366
Rahul N <rahul.grv.1026@gmail.com>
2026-03-07 00:43:29 +0530

    Fix: Unified workspace and cleaned Next.js build for Vercel

--- frontend/minimal-server.js
+++
@@ -1,4 +0,0 @@
-const express = require('express');
-const app = express();
-app.get('*', (req, res) => res.send('EXPRESS WORKING'));
-app.listen(3000, () => console.log('Minimal Express on 3000'));
